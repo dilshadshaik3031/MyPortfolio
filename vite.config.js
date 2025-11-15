d2f2f2b8af10ccda92683b47e7656c10b579b6c6
dilshad <dilshadshaik3031@gmail.com>
2025-11-15 04:48:36 -0600

    Fix CSS: changed base to relative

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio/',   // <- REQUIRED for GitHub Pages
+  base: './',        // ← IMPORTANT: Not /portfolio/
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 })
